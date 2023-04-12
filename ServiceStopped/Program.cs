05e90973b89e47fba677356eeb5c2a1e42e3383d
Sander Aiaots <sander.aiaots@helmes.com>
2023-04-12 15:00:16 +0300

    added service stopped check app

---
+++ ServiceStopped/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.ServiceProcess;
+using System.Threading;
+using ServiceStopped;
+
+if (args.Length == 0) {
+	Console.WriteLine("Please provide service name as first argument");
+	return;
+}
+string serviceName = args[0];
+Console.WriteLine($"Will check if service: '{args[0]}' is stopped");
+int i = 0;
+while (ServiceStatus.GetServiceStatus(serviceName) != ServiceControllerStatus.Stopped) {
+	if (i > 0) {
+		Console.Write(".");
+	}
+	Thread.Sleep(1000);
+	i++;
+}
+Console.WriteLine();
+Console.WriteLine($"Service {serviceName} is stopped");
---
+++ ServiceStopped/ServiceStatus.cs
@@ -0,0 +1,10 @@
+﻿using System.ServiceProcess;
+
+namespace ServiceStopped; 
+
+public class ServiceStatus {
+	public static ServiceControllerStatus GetServiceStatus(string serviceName) {
+		using ServiceController sc = new ServiceController(serviceName);
+		return sc.Status;
+	}
+}
